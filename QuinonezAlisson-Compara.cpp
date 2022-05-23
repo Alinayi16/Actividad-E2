9dd7f6cddcd88810a4514b668f1fe66ccedfc83e
Alisson Quiñonez <100954775+Alinayi16@users.noreply.github.com>
2022-05-23 18:09:39 -0500

    Examen

---
+++ QuinonezAlisson-Compara.cpp
@@ -0,0 +1,25 @@
+//==> Archivo :QuiñonezCanchingre-Compara.cpp
+//==> Nombre del programa: comparacion de dos numeros         
+//==>Autor: Alisson Quiñonez                                    
+//==>Fecha de elaboración:2022-04-29
+//==>Fecha de elaboración:2022-04-29
+//==>Fecha ultima actualización:2022-05-20
+//=================================================             
+# include <iostream>
+using namespace std;
+int main ()
+{
+        float ac_a,ac_b;
+        cout<<"ingrese los valores a b :"; cin>>ac_a>>ac_b;
+        if(ac_a==ac_b) {
+                cout<<"son iguales:";
+        }else
+                if(ac_a>ac_b) { cout<<"El mayor es"<<ac_a;
+                }else{ cout<<" El mayor es"<<ac_b;
+                }
+        
+        
+
+
+        return 0;
+}
